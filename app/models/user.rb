fe8de1f9f41717cd86a51783da081eaffdef31ec
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-04 16:16:47 -0500

    commit tree 8cf84d586a96ac5d864ea19ac43fed940876e4bb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+class User < ActiveRecord::Base
+    # attr_accessor :username, :password, :balance
 
+    # def initialize(params)
+    #     @username = params[:username]
+    #     @password = params[:password]
+    #     @balance = params[:balance]
+    # end
+end
 
